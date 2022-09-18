1601d903664823054b3ab15ac035472e6eefe6d9
Maksim Botalov <dev.botalov@gmail.com>
2022-09-18 18:02:42 +0500

    Initial Commit

---
+++ WordTypesetter/WordTypesetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordTypesetterApp.swift
+//  WordTypesetter
+//
+//  Created by Максим Боталов on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordTypesetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
